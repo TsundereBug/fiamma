0a46ff980648233b111eb607776edc70ca52c337
Anthony Cerruti <tsunderebug@tsunderebug.com>
2018-04-05 19:50:40 -0700

    Fix some scope issues

--- src/main/scala/com/tsunderebug/fiamma/Terminal.scala
+++ src/main/scala/com/tsunderebug/fiamma/Terminal.scala
@@ -6,12 +6,26 @@ object Terminal {
 
   def apply(implicit terminalFactory: lanterna.terminal.TerminalFactory): Terminal = new Terminal(terminalFactory.createTerminal())
 
+  private[Terminal] class ResizeListeners(terminal: Terminal) {
+
+    def +=(listener: lanterna.terminal.TerminalResizeListener): Unit = terminal.provider.addResizeListener(listener)
+    def -=(listener: lanterna.terminal.TerminalResizeListener): Unit = terminal.provider.removeResizeListener(listener)
+
+  }
+
+  private[Terminal] class SGRContainer(terminal: Terminal) {
+
+    def +=(sgr: SGR): Unit = terminal.provider.enableSGR(sgr.internal)
+    def -=(sgr: SGR): Unit = terminal.provider.disableSGR(sgr.internal)
+
+  }
+
 }
 
-class Terminal(provider: lanterna.terminal.Terminal) {
+class Terminal(private[Terminal] val provider: lanterna.terminal.Terminal) {
 
-  lazy val resizeListeners: ResizeListeners = new ResizeListeners
-  lazy val sgr: SGRContainer = new SGRContainer
+  lazy val resizeListeners: Terminal.ResizeListeners = new Terminal.ResizeListeners(this)
+  lazy val sgr: Terminal.SGRContainer = new Terminal.SGRContainer(this)
 
   def clear(): Unit = provider.clearScreen()
 
@@ -27,19 +41,5 @@ class Terminal(provider: lanterna.terminal.Terminal) {
     (pos.getColumn, pos.getRow)
   }
   def cursorPosition_=(position: (Int, Int)): Unit = provider.setCursorPosition(position._1, position._2)
-
-  private class ResizeListeners {
-
-    def +=(listener: lanterna.terminal.TerminalResizeListener): Unit = provider.addResizeListener(listener)
-    def -=(listener: lanterna.terminal.TerminalResizeListener): Unit = provider.removeResizeListener(listener)
-
-  }
-
-  private class SGRContainer {
-
-    def +=(sgr: SGR): Unit = provider.enableSGR(sgr.internal)
-    def -=(sgr: SGR): Unit = provider.disableSGR(sgr.internal)
-
-  }
 
 }
